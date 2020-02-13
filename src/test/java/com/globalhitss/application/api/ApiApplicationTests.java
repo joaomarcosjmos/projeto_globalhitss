005722d0d4488eecce11709a8da6cd55fc07d464
João Marcos <marcos.silva@ebix.com>
2020-02-13 00:12:48 -0300

    Implemtado conexão com o banco de dados MySQL

--- src/test/java/com/globalhitss/application/api/ApiApplicationTests.java
+++ src/test/java/com/globalhitss/application/api/ApiApplicationTests.java
@@ -1,6 +1,6 @@
 package com.globalhitss.application.api;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
